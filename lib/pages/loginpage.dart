a9f77b6276122f35effc6a5fe63b17c2a23c0dff
Deepshika Ghale <deepshikaghale99@gmail.com>
2021-07-10 22:55:34 +0545

    day 6 completed

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool change = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -14,7 +22,7 @@ class LoginPage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
             Text(
-              'Welcome',
+              'Welcome $name',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(
@@ -27,6 +35,11 @@ class LoginPage extends StatelessWidget {
                   TextFormField(
                     decoration: InputDecoration(
                         hintText: 'Enter Username', labelText: 'Username'),
+                    onChanged: (value) {
+                      name = value;
+                      setState(
+                          () {}); //reloads the build method, i.e refreshes the state, only works for stateful widgets
+                    },
                   ),
                   TextFormField(
                     obscureText: true, //hides the passsword
@@ -36,13 +49,43 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                    child: Text('Login'),
-                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    onPressed: () {
+                  InkWell(
+                    hoverColor: Colors.red,
+                    onTap: () async {
+                      setState(() {
+                      change = true;
+                      });
+                      
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: change ? 100: 150, // here, ? is the conditional expression which is given 
+                      //if the boolean data type tends to be true. the statement change ? 50 : 150 means that if the condition
+                      // is false then the width of the box will be 150 and likewise if the condition is false then the width will be 100
+
+                      height: 40,
+                      alignment: Alignment.center,
+                      child: change ? Icon(Icons.done, color: Colors.white): Text('Login',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15)),
+                      decoration: BoxDecoration(
+                        // shape: change? BoxShape.circle: BoxShape.rectangle,
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular( change ?20: 4),
+                      ),
+                    ),
                   )
+                  // ElevatedButton(
+                  //   child: Text('Login'),
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
                 ],
               ),
             )
