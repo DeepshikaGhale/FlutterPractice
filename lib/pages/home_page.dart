d8bbc89e0581df2b97bb42f7b16981d1ce96531f
Deepshika Ghale <deepshikaghale99@gmail.com>
2021-08-31 14:06:18 +0545

    gridview, gridtile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,7 +23,9 @@ class _HomePageState extends State<HomePage> {
         await rootBundle.loadString("assets/files/products.json");
     final decodeData = jsonDecode(productsJson);
     var productsdata = decodeData['products'];
-    CatalogModel.items = List.from(productsdata).map<Item>((item) => Item.fromMap(item)).toList();
+    CatalogModel.items = List.from(productsdata)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
     setState(() {});
   }
 
@@ -38,19 +40,48 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items.isNotEmpty)?  ListView.builder(
-          //here the builder renders the items shown on the screen
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) 
-            //here index means the number position like in an array
-            => ItemWidget(
-              item: CatalogModel.items[index],
-            )
-          ,
-        )
-        : Center(
-          child: CircularProgressIndicator(),
-          ),
+        child: (CatalogModel.items.isNotEmpty)
+            ?
+            // ListView.builder(
+            //   //here the builder renders the items shown on the screen
+            //   itemCount: CatalogModel.items.length,
+            //   itemBuilder: (context, index)
+            //     //here index means the number position like in an array
+            //     => ItemWidget(
+            //       item: CatalogModel.items[index],
+            //     )
+            //   ,
+            // )
+            GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 5),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: GridTile(
+                        header: Container(
+                        child: Text(item.name, style:  TextStyle(color: Colors.white),),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.green
+                        ),),
+                        child: Image.network(item.product_image),
+                        footer: Text(
+                          "\$${item.price.toString()}",
+                        ),
+                      ));
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
